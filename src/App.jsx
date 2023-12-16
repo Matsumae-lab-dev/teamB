f430c37359c270f963d51430a2ea90c422fb573f
Yamanakasuzuna <22238476@edu.cc.saga-u.ac.jp>
2023-12-17 05:00:00 +0900

    init: linter&prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 export default function App() {
   return (
-    <><h1 className="text-3xl font-bold text-rose-800">
-      Hello world!
-    </h1><button class="btn btn-primary">Button</button></>
-  )
+    <>
+      <h1 className="text-3xl font-bold text-rose-800">Hello world!</h1>
+      <button class="btn btn-primary">Button</button>
+    </>
+  );
 }
