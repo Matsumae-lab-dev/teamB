adb393ae7cb74db8e38a13f9b058bbaa849dc08a
nnnnn81 <nnnnnagi81@gmail.com>
2024-01-10 12:04:19 +0900

    change: component分離

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,11 @@
+import Button from '../components/Button'
 import './App.css'
 
 export default function App() {
   return (
     <>
       <h1 className='text-3xl font-bold text-rose-800'>Hello teamB!</h1>
-      <button className='btn btn-primary'>Button</button>
+      <Button />
     </>
   )
 }
