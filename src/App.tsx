f86941d2dd1a1c50c726846553c097db2ab4df28
nnnnn81 <nnnnnagi81@gmail.com>
2024-01-10 11:31:37 +0900

    init: typescript

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-rose-800'>Hello teamB!</h1>
+      <button className='btn btn-primary'>Button</button>
+    </>
+  )
+}
