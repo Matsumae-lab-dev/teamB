7b6e9931bbed49e009bd04d0f2525b9ba831c3ff
sgupge2366 <21238469@edu.cc.saga-u.ac.jp>
2024-01-16 03:48:47 +0900

    Todoページの編集

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,5 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function Todo() {
+  const navigate = useNavigate()
   return (
-    <h2>Todo</h2>
+    <>
+    <div className= "bg-[#AAD9BB] flex justify-around">
+      <div></div>
+      <h2 className="text-4xl" >TO DO</h2>
+      <button className= "btn m-2" onClick={() => navigate('/new')}>  +  </button>
+      
+      </div>
+    </>
   );
 }
