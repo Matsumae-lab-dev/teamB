b1a3385a7c425a5bb40ca88d92dd05b624b8e558
nnnnn81 <nnnnnagi81@gmail.com>
2024-01-17 10:11:36 +0900

    add: cookie

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import { BrowserRouter } from 'react-router-dom';
+import { CookiesProvider } from "react-cookie";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <CookiesProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CookiesProvider>
   </React.StrictMode>,
 );
