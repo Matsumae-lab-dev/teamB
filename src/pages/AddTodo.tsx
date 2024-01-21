cf3a0ee873e54caeb475eacbb0d3041efc065e1a
Yamanakasuzuna <22238476@edu.cc.saga-u.ac.jp>
2024-01-21 13:58:57 +0900

    konnnitiha

--- src/pages/AddTodo.tsx
+++ src/pages/AddTodo.tsx
@@ -1,5 +1,14 @@
+import Todolist from "../components/Todolist";
+
 export default function AddTodo() {
   return (
-    <h2>AddTodo</h2>
+    <>
+      <h2>AddTodo</h2>
+      <div className="rennsyuu2">
+        <Todolist />
+      </div>
+        
+    </>
+    
   );
 }
