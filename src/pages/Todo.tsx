65a6adc9dff08ba9cac5508428507693fc67984c
nnnnn81 <nnnnnagi81@gmail.com>
2024-01-21 12:37:26 +0900

    fix: fetchの仕方変えた

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,39 +1,52 @@
 import { useNavigate } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
-import { useEffect, useState } from 'react';
 import axios from 'axios';
+import useSWR from 'swr';
+
+type TodoData = {
+  Id: number;
+  Title: string;
+  Content: string;
+  CreatedAt: string;
+  UpdatedAt: string;
+  Deadline: string;
+  Tag: string;
+  TagColor: string;
+  CreaterId: number;
+  RepeatFlag: boolean;
+  RepeatSpan: number;
+  RepeatCount: number;
+};
+
 
 export default function Todo() {
-  const [data, setData] = useState([]);
   const [cookies, _, removeCookies] = useCookies(['token']);
   const navigate = useNavigate()
 
-  useEffect(() => {
-    if (!cookies.token) {
-      navigate('/')
+  const fetcher = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/auth/todo', {
+        headers: {
+          Authorization: cookies.token ? `Bearer ${cookies.token}` : '',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      throw error;
     }
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:8080/auth/todo', {
-          headers: {
-            Authorization: `Bearer ${cookies.token}`,
-          },
-        });
-        const resdata = response.data;
-        if (resdata != null) {
-          setData(resdata.todos);
-          console.log(data)
-        } else {
-          // ダミーデータを詰めとく
-        }
+  };
 
-      } catch (error: any) {
-        console.error('Error:', error.message);
-      }
-    };
+  const { data, error } = useSWR('http://localhost:8080/auth/todo', fetcher);
 
-    fetchData();
-  }, [cookies.token]);
+  if (!cookies.token) {
+    navigate('/');
+    return null;
+  }
+
+  if (error) {
+    console.error('Error:', error.message);
+    return <div>Error loading data</div>;
+  }
 
   const logout = () => {
     removeCookies('token')
@@ -46,8 +59,21 @@ export default function Todo() {
         <h2 className="text-4xl" >TO DO</h2>
         <button className="btn m-2" onClick={() => navigate('/new')}>  +  </button>
         <button onClick={logout}>logout</button>
-        <div>
-        </div>
+      </div>
+      <div>
+        {data ? (
+          <ul>
+            {data.todos.map((item: TodoData) => (
+              <li key={item.Id}>
+                <h3>{item.Title}</h3>
+                <p>Tag: {item.Tag}</p>
+                <p>TagColor: {item.TagColor}</p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div>Loading...</div>
+        )}
       </div>
     </>
   );
