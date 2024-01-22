cc3d54956dd40688c74884224c17e32c9b948d36
nnnnn81 <nnnnnagi81@gmail.com>
2024-01-22 09:54:07 +0900

    tagごとに分けて表示

--- src/components/Todolist2.tsx
+++ src/components/Todolist2.tsx
@@ -1,27 +1,33 @@
 import React from 'react';
 import Todolist from "../components/Todolist";
 import TodoButton from "./TodoButton";
+import { TodoData } from '../pages/Todo';
 
-export default function list() {
+interface tododata {
+  todos: TodoData[]
+}
+
+export default function list({ todos }: tododata) {
   return (
     <>
-      <div
-        tabIndex={0}
-        className="collapse collapse-arrow bg-base-200 rounded-box"
-      >
-        <input type="checkbox" className="peer" />
-        <div className="bg-white collapse-title text-primary-content">
-          <div className="list">
-            <Todolist />
+      {todos.map((item: TodoData) => (
+        <div
+          tabIndex={0}
+          className="collapse collapse-arrow bg-base-200 rounded-box"
+        >
+          <input type="checkbox" className="peer" />
+          <div className="bg-white collapse-title text-primary-content">
+            <div className="list">
+              <Todolist category={item.Tag} />
+            </div>
+          </div>
+          <div className="bg-white collapse-content text-primary-content">
+            <div>
+              <TodoButton todo={item} />
+            </div>
           </div>
         </div>
-        <div className="bg-white collapse-content text-primary-content">
-          <div className="Button">
-            <TodoButton />
-          </div>
-        </div>
-
-      </div>
+      ))}
     </>
   )
 }
